fd93eb5dc0c0ffb2b22da3381b632ec491fa1202
AlexMaUr <maltsev.a.lightit@gmail.com>
2019-12-19 10:00:51 +0200

    feat (shared): add material modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,16 +3,28 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
+import {
+  MatButtonModule,
+  MatInputModule,
+} from '@angular/material';
+
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
+    // material modules
+    MatButtonModule,
+    MatInputModule,
   ],
   exports: [
     CommonModule,
     ReactiveFormsModule,
     RouterModule,
+    // material modules
+    MatButtonModule,
+    MatInputModule,
   ],
 })
-export class SharedModule { }
+export class AppSharedModule {
+}
