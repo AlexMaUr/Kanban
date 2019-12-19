6977a39c5953c7eee97f0464e2e3979d1ff78e5e
AlexMaUr <maltsev.a.lightit@gmail.com>
2019-12-19 10:26:51 +0200

    feat (main): create main module

---
+++ src/app/main/main-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MainComponent } from './main.component';
+
+
+const routes: Routes = [{
+  path: '',
+  component: MainComponent,
+  children: [
+    {
+      path: '',
+      loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+    },
+  ],
+}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class MainRoutingModule {
+}
