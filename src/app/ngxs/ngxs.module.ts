f56a1d00e27decfdddfbbec1c4daf14afb1af932
AlexMaUr <maltsev.a.lightit@gmail.com>
2019-12-19 13:16:13 +0200

    feat (ngxs): add auth states

--- src/app/ngxs/ngxs.module.ts
+++ src/app/ngxs/ngxs.module.ts
@@ -4,11 +4,20 @@ import { NgxsModule } from '@ngxs/store';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 
 import { environment } from '../../environments/environment';
+import { AuthState } from './auth';
+import { RequestsState } from './requests/requests.state';
+import { SignInRequestState } from './requests/auth/sign-in/sign-in-request.state';
+import { SignUpRequestState } from './requests/auth/sign-up/sign-up-request.state';
 
 
 @NgModule({
   imports: [
     NgxsModule.forRoot([
+      AuthState,
+      // requests
+      RequestsState,
+      SignInRequestState,
+      SignUpRequestState,
     ]),
     NgxsReduxDevtoolsPluginModule.forRoot({
       disabled: environment.production
