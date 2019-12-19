00f9ea7317da3a8e575d2087cfb75c7359fe9b3c
AlexMaUr <maltsev.a.lightit@gmail.com>
2019-12-19 10:29:25 +0200

    feat (core): add validation const to providers

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
-
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgxsStoreModule } from '../ngxs/ngxs.module';
 import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
 import { environment } from '../../environments/environment';
+import { VALIDATION, VALIDATION_VALUE } from './constants/validation.const';
 
 
 @NgModule({
@@ -12,7 +16,15 @@ import { environment } from '../../environments/environment';
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     BrowserAnimationsModule,
     NgxsStoreModule,
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
-  ]
+  ],
+  providers: [
+    {
+      provide: VALIDATION,
+      useValue: VALIDATION_VALUE,
+    },
+  ],
 })
 export class CoreModule { }
